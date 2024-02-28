ae6622c1263aa3aa98532ab2b26af6c3a067add0
John Morales <john@johnmorales.dev>
2024-02-28 18:39:43 -0500

    Fixed miscalculation

--- Assignment_P2/src_python/payroll_calculator.py
+++ Assignment_P2/src_python/payroll_calculator.py
@@ -5,21 +5,21 @@ _cdecFed = Decimal(0.22)
 _cdecState = Decimal(0.04)
 
 def calculate_state_tax(pay, rate=_cdecState):
-    return pay*rate
+    return Decimal(pay*rate).quantize(Decimal('1.00'))
 
 def calculate_fica(pay, rate=_cdecFica):
-    return pay*rate
+    return Decimal(pay*rate).quantize(Decimal('1.00'))
 
 def calculate_federal_tax(pay, rate=_cdecFed):
-    return pay*rate
+    return Decimal(pay*rate).quantize(Decimal('1.00'))
 
 def calculate_net_income(pay):
-    dec_pay = Decimal(pay)
+    dec_pay = Decimal(pay).quantize(Decimal('1.00'))
     state_tax = calculate_state_tax(dec_pay)
     fica_amount = calculate_fica(dec_pay)
     fed_tax = calculate_federal_tax(dec_pay)
     net_income = dec_pay - (state_tax + fica_amount + fed_tax)
-    return state_tax.quantize(Decimal('1.00')), fica_amount.quantize(Decimal('1.00')), fed_tax.quantize(Decimal('1.00')), net_income.quantize(Decimal('1.00'))
+    return state_tax, fica_amount, fed_tax, net_income
 
 def test_net_income():
     results = calculate_net_income(123456.78)
